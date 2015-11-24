03bd940243f904c788b9daa0f8d0e01045eb0ff9
Alvis Logins <alvis@mit.edu>
2015-11-24 01:52:48 +0000

    sorting fix

--- player/search.c
+++ player/search.c
@@ -144,10 +144,10 @@ static score_t searchPV(searchNode *node, int depth, uint64_t *node_count_serial
 
   moveEvaluationResult* result = (moveEvaluationResult *) malloc(sizeof(moveEvaluationResult));
 
+    // Incrementally sort the move list.
+    sort_incremental(move_list, num_of_moves, 0);
   // Start searching moves.
   for (int mv_index = 0; mv_index < num_of_moves; mv_index++) {
-    // Incrementally sort the move list.
-    sort_incremental(move_list, num_of_moves, mv_index);
 
     move_t mv = get_move(move_list[mv_index]);
 
