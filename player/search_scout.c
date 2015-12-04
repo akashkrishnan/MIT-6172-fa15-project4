632c6fc37419b6d81ef3be74fd0437bfc614322f
Alvis Logins <alvis@mit.edu>
2015-12-04 18:41:52 -0500

    fix merge 2

--- player/search_scout.c
+++ player/search_scout.c
@@ -132,50 +132,11 @@ static score_t scout_search(searchNode *node, int depth,
         bool cutoff = search_process_score(node, mv, local_index, &result, SEARCH_SCOUT);
         if (cutoff) {
           node->abort = true;
-        simple_release(&node_mutex);
+          simple_release(&node_mutex);
           continue;
         }
         simple_release(&node_mutex);
     } while (false);
-=======
-  for (int mv_index = 0; mv_index < num_of_moves; mv_index++) {
-    if (mv_index == 1) {
-      sort_incremental_full(move_list, num_of_moves, number_of_moves_evaluated);
-    }
-    // Get the next move from the move list.
-    int local_index = number_of_moves_evaluated++;
-    move_t mv = get_move(move_list[local_index]);
-
-    if (TRACE_MOVES) {
-      print_move_info(mv, node->ply);
-    }
-
-    // increase node count
-    __sync_fetch_and_add(node_count_serial, 1);
-
-    evaluateMove(&result, node, mv, killer_a, killer_b,
-                                               SEARCH_SCOUT,
-                                               node_count_serial);
-
-    if (result.type == MOVE_ILLEGAL || result.type == MOVE_IGNORE
-        || abortf || parallel_parent_aborted(node)) {
-      continue;
-    }
-
-    // A legal move is a move that's not KO, but when we are in quiescence
-    // we only want to count moves that has a capture.
-    if (result.type == MOVE_EVALUATED) {
-      node->legal_move_count++;
-    }
-
-    // process the score. Note that this mutates fields in node.
-    bool cutoff = search_process_score(node, mv, local_index, &result, SEARCH_SCOUT);
-
-    if (cutoff) {
-      node->abort = true;
-      break;
-    }
->>>>>>> 2a0078ff3a5f5d6b841294b96f8bdba8f776d081
   }
 
   if (parallel_parent_aborted(node)) {
